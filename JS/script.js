08f309fbdf54b43e7cd814a4be1786b35f6867a7
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2024-04-11 01:57:21 +0000

    Add input field for box size -Initialize box size to 16 on page load -Include two buttons (rainbow and dark) for color selection.

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,57 @@
 let container = document.querySelector(".container");
-let gridSize = 20;
-for (let i=0; i<gridSize*gridSize; i++) {
-  let box = document.createElement("div");
-  box.style.width= "calc(100% / 20)";
-  box.style.height= "calc(100% / 20)";
-  box.addEventListener("mouseover", () => {
-    box.style.backgroundColor=randomColor();
-  });
-  box.addEventListener("mouseout", () => {
-    box.style.backgroundColor= "white";
-  });
-  box.addEventListener("click", () => {
-    box.style.backgroundColor="black";
-  });
-  
-  box.classList.add("box");
-  container.appendChild(box);
+let gridSize = 16;
+    getGrid();
+
+document.querySelector("#gridSizeInput").addEventListener("change", function() {
+    gridSize = parseInt(this.value); 
+    getGrid(); 
+});
+
+function getGrid(){
+
+    if(gridSize > 100){
+        gridSize = 100;
+    }
+    if(gridSize < 4){
+        gridSize = 4;
+    }
+
+    container.innerHTML = '';
+
+    for (let i=0; i<gridSize*gridSize; i++) {
+        let box = document.createElement("div");
+        box.style.width= "calc(100% /" + gridSize + ")";
+        box.style.height= "calc(100% /" + gridSize + ")";
+
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor=randomColor();
+        });
+        box.addEventListener("mouseout", () => {
+            setTimeout(() => {
+            box.style.backgroundColor = "white";
+            }, 200); 
+        });
+        box.addEventListener("click", () => {
+            box.style.backgroundColor = "black";
+        });
+        box.classList.add("box");
+        container.appendChild(box);
+        
+        
+
+    }
 }
 
 
-
-
-//fonction to generate random RGB color
 function randomColor() {
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  return "rgb(" +r+ "," + g + "," + b + ")";
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" +r+ "," + g + "," + b + ")";
 }
 
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        getGrid();
+    }
+}
