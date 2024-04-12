8cf709f60040cb19a17c38a34e70480e3d07d030
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2024-04-12 15:03:33 +0000

    -Fixe bug related to rainbowColorButton & darkColorButton click -fixe bug related to eraseButton click

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,18 @@
 // Initialize variables and generate grid on page load
 let container = document.querySelector(".container");
+const pick = document.querySelector("#pick");
 let gridSize = 16;
+
 getGrid();
 
 document.querySelector("#gridSizeInput").addEventListener("change", function() {
     gridSize = parseInt(this.value); 
     getGrid(); 
 });
+
+
+
+
 function getGrid() {
     if(gridSize > 50){
         gridSize = 50;
@@ -18,76 +24,120 @@ function getGrid() {
         gridSize = 16;
     }
 
-    container.innerHTML = '';
-
+    container.innerHTML = "";
     for (let i=0; i<gridSize*gridSize; i++) {
+
         let box = document.createElement("div");
         box.style.width= "calc(100% /" + gridSize + ")";
         box.style.height= "calc(100% /" + gridSize + ")";
-        rainbowColor();
-        showGrid();
+        box.style.backgroundColor= "white";
         box.classList.add("box");
         container.appendChild(box);
 
 
-
+        showGrid();
 
         let rainbowColorButton = document.querySelector("#rainbowColor");
-        let isMouseOverEnabled = true;
-
-
-        function rainbowColor(){
-            box.addEventListener("mouseover", function() {
-                if (isMouseOverEnabled) {
-                    box.style.backgroundColor=randomColor();
-                            setTimeout(() => {
-                                box.style.backgroundColor = "white";
-                                }, 200);
-                }
-            });
-            box.addEventListener("click", () => {
-                isMouseOverEnabled = false;
-                box.style.backgroundColor = randomColor();
-
-            });
-            container.classList.remove("eraser");
-            container.classList.add("brush");
-        }
-        rainbowColorButton.addEventListener("click", rainbowColor);       
-
         let darkColorButton = document.querySelector("#darkColor");
-        function  darkColor(){
-            
-            box.addEventListener("click", () => {
-                isMouseOverEnabled = false;
-                box.style.backgroundColor = "black";
-            });
-            box.addEventListener("mouseover", () => {
-                if (isMouseOverEnabled) {
-                box.style.backgroundColor=generateGrayShade();
-                setTimeout(() => {
-                    box.style.backgroundColor = "white";
-                    }, 200); 
+        let isDrawing = false;
+        
+        function handleColorMode(event) {
+            const box = event.target;
+            if (box.classList.contains('box')) {
+                if (container.classList.contains('rainbow')) {
+                    if (!isDrawing && box.style.backgroundColor === 'white') {
+                        box.style.backgroundColor = randomColor();
+                        setTimeout(() => {
+                            box.style.backgroundColor = 'white';
+                        }, 200);
+                    }
+                } else if (container.classList.contains('dark')) {
+                    if (!isDrawing && box.style.backgroundColor === 'white') {
+                        box.style.backgroundColor = generateGrayShade();
+                        setTimeout(() => {
+                            box.style.backgroundColor = 'white';
+                        }, 200);
+                    }
                 }
-            });
-            container.classList.remove("eraser");
-            container.classList.add("brush");
+            }
         }
-        darkColorButton.addEventListener("click", darkColor);
-
+        
+        container.addEventListener('mouseover', handleColorMode);
+        container.addEventListener('click', handleColorMode);
+        container.addEventListener('mousedown', () => {
+            isDrawing = true;
+        });
+        container.addEventListener('mouseup', () => {
+            isDrawing = false;
+        });
+        container.addEventListener('mousemove', event => {
+            const box = event.target;
+            if (isDrawing && box.classList.contains('box') && box.style.backgroundColor === 'white') {
+                if (container.classList.contains('rainbow')) {
+                    box.style.backgroundColor = randomColor();
+                } else if (container.classList.contains('dark')) {
+                    box.style.backgroundColor = generateGrayShade();
+                }
+            }
+        });
+        
+        function setRainbowColor() {
+            container.classList.remove('dark');
+            container.classList.add('rainbow');
+            container.classList.add("brush");
+            container.classList.remove('eraser');
+        }
+        
+        rainbowColorButton.addEventListener('click', setRainbowColor);
+        rainbowColorButton.click();
+        
+        function setDarkColor() {
+            container.classList.remove('rainbow');
+            container.classList.add('dark');
+            container.classList.add("brush");
+            container.classList.remove('eraser');
+        }
+        
+        darkColorButton.addEventListener('click', setDarkColor);
+        
 
 
 // Erase and reset buttons
-        let eraseButton = document.querySelector("#erase");
-        function erase (){
-            container.classList.remove("brush");
-            container.classList.add('eraser')
-            box.addEventListener("click", () => {
-                isMouseOverEnabled = false;
-                box.style.backgroundColor = "white";
-            });
+let eraseButton = document.querySelector("#erase");
+
+function erase() {
+    container.classList.remove("brush");
+    container.classList.add('eraser');
+    // Remove the event listener
+    container.classList.remove('rainbow');
+    container.classList.remove('dark');
+
+    container.addEventListener('click', event => {
+        const box = event.target;
+        if (box.classList.contains('box') && box.style.backgroundColor !== "white") {
+            box.style.backgroundColor = "white";
         }
-        eraseButton.addEventListener("click", erase);
+    });
+
+    container.addEventListener('mousedown', () => {
+        isDrawing = true;
+    });
+
+    container.addEventListener('mouseup', () => {
+        isDrawing = false;
+    });
+
+    container.addEventListener('mousemove', event => {
+        const box = event.target;
+        if (isDrawing && box.classList.contains('box') && box.style.backgroundColor !== 'white') {
+            box.style.backgroundColor = 'white';
+        }
+    });  
+}
+
+eraseButton.addEventListener("click", erase);
+
+
 
         let resetButton = document.querySelector("#reset");
         function reset(){
@@ -115,7 +165,7 @@ function getGrid() {
 
 // Generate black and white shades
 function generateGrayShade() {
-    let Rnum = Math.floor(Math.random() * 220);
+    let Rnum = Math.floor(Math.random() * 200); 
     return "rgb(" + Rnum +"," + Rnum + "," + Rnum + ")";
 }
 
